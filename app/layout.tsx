f781c5f26b27f30d7a49a39a2b4db12d5c4a046e
cwpslxck <cwpslxck@gmail.com>
2025-06-04 04:48:27 +0330

    Building: Daily update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} antialiased  py-20 md:py-28`}>
-        <div className="max-w-xs md:max-w-2xl mx-auto border-t border-x border-black/10">
+      <body className={`${font.className} antialiased px-2 py-6 md:py-28`}>
+        <div className="w-full md:max-w-2xl md:mx-auto border-t border-x border-black/10">
           {children}
         </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CalculateAge from "@/Components/calculateAge";
 import Section from "@/Components/section";
 import Image from "next/image";
+import { Mail } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   const skills = [
@@ -16,9 +18,9 @@ export default function Home() {
   return (
     <>
       <Section>
-        <div className="flex justify-between items-start p-6">
+        <div className="flex justify-between items-start p-2 md:p-6">
           <div className="w-[80%]">
-            <p className="text-6xl font-black">Hi, i'm Amin👋🏻</p>
+            <p className="text-3xl md:text-6xl font-black">Hi, i'm Amin👋🏻</p>
             <p className="text-xl">
               <CalculateAge />
               -year-old digital creator, frontend developer
@@ -41,7 +43,11 @@ export default function Home() {
       <Section title="About">
         <p className="opacity-85">
           I'm currently working on{" "}
-          <a href="https://x.com/yxnstudio" target="_blank">
+          <a
+            className="external-links"
+            href="https://x.com/yxnstudio"
+            target="_blank"
+          >
             @YXNstudio
           </a>
           . A platform for new-generation artists that help them manage theire
@@ -61,56 +67,35 @@ export default function Home() {
         </div>
       </Section>
       <Section classNameWrapper="flex flex-col gap-4" title="Work Experience">
-        <div className="w-full border bg-white border-black/10 rounded-md min-h-28 flex justify-between items-start p-4 gap-4  ">
+        <Link
+          href={"https://x.com/yxnstudio"}
+          target="_blank"
+          className="w-full border bg-white border-black/10 rounded-md min-h-28 flex justify-between items-start p-4 gap-4  "
+        >
           <div className="w-12">
             <Image
               draggable="false"
-              className="rounded-full"
-              alt="Profile Picture"
-              src={"https://github.com/cwpslxck.png"}
-              width={100}
-              height={100}
+              className="rounded-full aspect-square object-contain"
+              alt="Brand Picture"
+              src={"/images/yxnstudio.jpg"}
+              width={60}
+              height={60}
             />
           </div>
           <div className="w-full h-full">
-            <div className="w-full justify-between flex items-start pb-2">
+            <div className="w-full justify-between flex pb-2">
               <div>
-                <p className="font-extrabold">LearnPOV</p>
-                <p className="text-sm">1/2 of Frontend Team</p>
+                <p className="font-extrabold">YXN Studio</p>
+                <p className="text-sm">Founder</p>
               </div>
-              <div className="text-sm opacity-80">April 2025 - Present</div>
+              <div className="text-sm opacity-80">July 2023 - Present</div>
             </div>
             <div>
               jfkds lkdjflsj fldks jfs j lfjdsj fljdf lsdjf lksdjf lkdsjf
               lkdjkldfj slkdfj sdlkfj slkjslkf jsdlk sldkjflk sj
             </div>
           </div>
-        </div>
-        <div className="w-full border bg-white border-black/10 rounded-md min-h-28 flex justify-between items-start p-4 gap-4  ">
-          <div className="w-12">
-            <Image
-              draggable="false"
-              className="rounded-full"
-              alt="Profile Picture"
-              src={"https://github.com/cwpslxck.png"}
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="w-full h-full">
-            <div className="w-full justify-between flex items-start pb-2">
-              <div>
-                <p className="font-extrabold">LearnPOV</p>
-                <p className="text-sm">1/2 of Frontend Team</p>
-              </div>
-              <div className="text-sm opacity-80">April 2025 - Present</div>
-            </div>
-            <div>
-              jfkds lkdjflsj fldks jfs j lfjdsj fljdf lsdjf lksdjf lkdsjf
-              lkdjkldfj slkdfj sdlkfj slkjslkf jsdlk sldkjflk sj
-            </div>
-          </div>
-        </div>
+        </Link>
       </Section>
       <Section>
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical -mb-8">
@@ -159,7 +144,11 @@ export default function Home() {
               <div className="text-lg font-black">on my way to coding</div>
               Covid kickes in and i had a lots of time to try new things :)
               <br />I figured out how{" "}
-              <a target="_blank" href="https://multitheftauto.com/">
+              <a
+                className="external-links"
+                target="_blank"
+                href="https://multitheftauto.com/"
+              >
                 Mta
               </a>{" "}
               game works
@@ -240,6 +229,14 @@ export default function Home() {
             {/* <hr /> */}
           </li>
         </ul>
+      </Section>
+      <Section>
+        <div className="flex flex-col items-center gap-3 py-6">
+          <p className="text-3xl md:text-6xl font-black">Get In Touch</p>
+          <Link href={"mailto:cwpslxck@gmail.com"}>
+            <Mail />
+          </Link>
+        </div>
       </Section>
     </>
   );
