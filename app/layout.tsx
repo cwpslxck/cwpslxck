c0a6d49c4e586f120d6238e49a596ac93f7b2b56
cwpslxck <cwpslxck@gmail.com>
2025-06-05 16:48:02 +0330

    Update: add twitter card preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import { Home } from "lucide-react";
 import Footer from "@/Components/footer";
 
 const font = Space_Grotesk({
@@ -11,6 +10,12 @@ const font = Space_Grotesk({
 export const metadata: Metadata = {
   title: "Amin Zare",
   description: "Portfolio website of cwpslxck",
+  twitter: {
+    card: "summary_large_image",
+    images: "/assets/preview.png",
+    title: "Amin Zare",
+    description: "portfolio website of @cwpslxck",
+  },
 };
 
 export default function RootLayout({
