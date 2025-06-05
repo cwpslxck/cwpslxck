208f992f79f1dfb20e91377928c0e5f544133a70
cwpslxck <cwpslxck@gmail.com>
2025-06-05 16:17:13 +0330

    Update: Social media status numbers

--- app/page.tsx
+++ app/page.tsx
@@ -133,19 +133,19 @@ export default function Home() {
         <div className="pt-8 grid grid-cols-1 md:grid-cols-3 gap-12">
           <div className="flex justify-center items-center flex-col">
             <div className="text-7xl font-extrabold tracking-tight drop-shadow-xl">
-              61
+              60+
             </div>
             <p>Github Followers</p>
           </div>
           <div className="flex justify-center items-center flex-col">
             <div className="text-7xl font-extrabold tracking-tight drop-shadow-xl">
-              7
+              7+
             </div>
             <p>Twitch Follower</p>
           </div>
           <div className="flex justify-center items-center flex-col">
             <div className="text-7xl font-extrabold tracking-tight drop-shadow-xl">
-              22
+              20+
             </div>
             <p>Youtube Videos</p>
           </div>
