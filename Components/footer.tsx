c94e35fdbb86baeab6091ee4bf0f1b1329e6af8e
cwpslxck <cwpslxck@gmail.com>
2025-08-03 16:16:22 +0330

    Home: fix mobile footer padding

--- Components/footer.tsx
+++ Components/footer.tsx
@@ -5,7 +5,7 @@ import { LuNotebookText } from "react-icons/lu";
 
 function Footer() {
   return (
-    <div className="w-full right-0 left-0 pb-4 z-50 flex justify-center fixed bottom-0 bg-gradient-to-b from-transparent to-white">
+    <div className="w-full px-2 md:px-0 right-0 left-0 pb-4 z-50 flex justify-center fixed bottom-0 bg-gradient-to-b from-transparent to-white">
       <div className="bg-white/70 px-4 border text-black max-w-xl text-xl border-black/10 backdrop-blur-xl rounded-full h-14 w-full flex justify-between items-center">
         <Link
           className="size-12 rounded-full flex items-center justify-center"
