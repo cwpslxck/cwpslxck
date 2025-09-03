e7cc6965e038e1e4bf75ae81cb2a409345a818dc
cwpslxck <cwpslxck@gmail.com>
2025-09-03 04:01:04 +0330

    GALLERY PAGE: add images

---
+++ constants/gallery.ts
@@ -0,0 +1,34 @@
+export const GALLERY_ITEMS = [
+  {
+    title: "معلمان نمونه ناحیه دو شیراز 1403 - 1404",
+    url: "/gallery/1.png",
+  },
+  {
+    title: "UI post card design for @TRXPFA",
+    url: "/gallery/2.png",
+  },
+  {
+    title: "Logo design for @YXNSTUDIO",
+    url: "/gallery/3.png",
+  },
+  {
+    title: "Sticker design for @VLONEFARSI",
+    url: "/gallery/4.png",
+  },
+  {
+    title: "Character design for personal brand",
+    url: "/gallery/5.jpg",
+  },
+  {
+    title: "Hiring slide banner for @YXNSTUDIO",
+    url: "/gallery/6.png",
+  },
+  {
+    title: "Story banner for FINGER CODER: CODE NEST",
+    url: "/gallery/7.png",
+  },
+  {
+    title: "LinkedIn header for my personal account",
+    url: "/gallery/8.png",
+  },
+];
